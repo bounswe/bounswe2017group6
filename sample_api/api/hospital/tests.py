faf070e1e88415479078416ecd1a9dc5e03ff4fe
Ahmet Ozan Tatlısu <ahmetozan789@gmail.com>
2017-05-08 21:47:37 +0300

    unit tests for get patient and post doctor methods

--- sample_api/api/hospital/tests.py
+++ sample_api/api/hospital/tests.py
@@ -1,3 +1,16 @@
+import unittest
+import os
+import subprocess
 from django.test import TestCase
 
-# Create your tests here.
+class Test2(TestCase):
+def test_patient_get(self):
+ 		patientName = subprocess.getoutput('http --json GET http://127.0.0.1:8000/hospital/patient/1/')
+ 		expected='{"Patient": "qwer qwerty"}
+ 		self.assertEqual(expected,patientName)
+class Test3(TestCase):
+def test_doctor_post(self):
+ 		doctorName = subprocess.getoutput('http --json POST http://127.0.0.1:8000/hospital/doctor/ doctor_id=2 name="mehmet" lastname="kısa" age=41')
+ 		expected='{"message": "doctor added", "status": "OK"}'
+ 		self.assertEqual(expected,doctorName)
+ 		
