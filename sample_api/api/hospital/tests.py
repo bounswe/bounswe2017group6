253605784a5490946fba92828901116b2af4574b
emreshome <emreshome@gmail.com>
2017-05-08 21:15:36 +0300

    unittest for rendezvous(get,post) added

--- sample_api/api/hospital/tests.py
+++ sample_api/api/hospital/tests.py
@@ -1,3 +1,17 @@
+import unittest
+import os
+import subprocess
 from django.test import TestCase
 
-# Create your tests here.
+class Test1(TestCase):
+	def test_rendezvous_get(self):
+		result = subprocess.getoutput('http --json GET http://127.0.0.1:8000/hospital/rendezvous/1/')
+		expected='{"Patient": "qwer qwerty", "Doctor Name": "Kazim Kazim", "Time": "2017-04-23 22:07:39.117858+00:00"}'
+		self.assertEqual(expected,result)
+	def test_rendezvous_get(self):
+		result = subprocess.getoutput('http --json POST http://127.0.0.1:8000/hospital/rendezvous/ patient_id=3 doctor_id=1')
+		expected='{"message": "rendezvous added", "status": "OK"}'
+		self.assertEqual(expected,result)
+		
+		
+		
